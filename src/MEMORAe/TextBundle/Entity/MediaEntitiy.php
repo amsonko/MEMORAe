0e744c3c3efae2c980d4277ed2824abd1f464423
amsonko <93asonko@gmail.com>
2015-05-01 21:13:16 +0200

    Mis à jour du script de BDD:Création des entity: admin, media et page

---
+++ src/MEMORAe/TextBundle/Entity/MediaEntitiy.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace MEMORAe\TextBundle\Entity;
+
+/**
+ * Description of MediaEntitiy: classe entity pour la table Media
+ *
+ * @author Amstrong
+ */
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Media")
+ */
+
+class MediaEntitiy {
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="media_id", type="bigint")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="media_pk_seq")
+     */
+    private $id;   
+    /**
+     * @ORM\Column(name="media_name", type="string", length=255)
+     */
+    private $name;
+    
+    /**
+     * @ORM\Column(name="media_path", type="text")
+     */
+    private $path;
+    
+    /**
+     * @ORM\Column(name="media_content", type="text")
+     */
+    private $content;
+    
+    /**
+     * @ORM\Column(name="current_media", type="string")
+     */
+    private $type;
+    
+    /**
+     * @ORM\Column(name="media_language", type="string")
+     */
+    private $language;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="PageEntity", inversedBy="medias")
+     * @ORM\JoinColumn(name="page_id", referencedColumnName="page_id", nullable=false)
+     **/
+    private $page;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return MediaEntitiy
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return MediaEntitiy
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return MediaEntitiy
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return MediaEntitiy
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set language
+     *
+     * @param string $language
+     * @return MediaEntitiy
+     */
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * Get language
+     *
+     * @return string 
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * Set page
+     *
+     * @param \MEMORAe\TextBundle\Entity\PageEntity $page
+     * @return MediaEntitiy
+     */
+    public function setPage(\MEMORAe\TextBundle\Entity\PageEntity $page = null)
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    /**
+     * Get page
+     *
+     * @return \MEMORAe\TextBundle\Entity\PageEntity 
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+}
---
+++ src/MEMORAe/TextBundle/Entity/PageEntity.php
@@ -0,0 +1,109 @@
+<?php
+namespace MEMORAe\TextBundle\Entity;
+/**
+ * Classe entity des pages
+ *
+ * @author Amstrong
+ */
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Media")
+ */
+
+class PageEntity {
+    
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="page_id", type="bigint")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="page_pk_seq")
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(name="page_name", type="string", length=255)
+     */
+    private $name;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="MediaEntity", mappedBy="page")
+     */
+    private $medias;
+    
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->medias = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return PageEntity
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add medias
+     *
+     * @param \MEMORAe\TextBundle\Entity\MediaEntity $medias
+     * @return PageEntity
+     */
+    public function addMedia(\MEMORAe\TextBundle\Entity\MediaEntity $medias)
+    {
+        $this->medias[] = $medias;
+
+        return $this;
+    }
+
+    /**
+     * Remove medias
+     *
+     * @param \MEMORAe\TextBundle\Entity\MediaEntity $medias
+     */
+    public function removeMedia(\MEMORAe\TextBundle\Entity\MediaEntity $medias)
+    {
+        $this->medias->removeElement($medias);
+    }
+
+    /**
+     * Get medias
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getMedias()
+    {
+        return $this->medias;
+    }
+}
