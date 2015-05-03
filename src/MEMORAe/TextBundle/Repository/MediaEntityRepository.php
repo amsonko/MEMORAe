e320e502df6699bed2c9119687fcf5950e310fc9
marlihams <mohamedmadioud@yahoo.fr>
2015-05-03 22:32:37 +0200

    mise à jour de MediaEntityRepository pour la récupartion de toutes les données de toutes les pages

--- src/MEMORAe/TextBundle/Repository/MediaEntityRepository.php
+++ src/MEMORAe/TextBundle/Repository/MediaEntityRepository.php
@@ -8,16 +8,24 @@ use Doctrine\ORM\EntityRepository;
 class MediaEntityRepository extends EntityRepository
 {
     
-    public function findMediaCurrentpage($id){
-     $qb = $this->createQueryBuilder('a');
+    public function findAllMedia(){
+    $home=findMediaForOnePage(1);
+    $qMemorae=findMediaForOnePage(2);
+    return array("home" => $home,
+                 "qMemerae" => $qMemorae
+                );
+  
+}
+    private function findMediaForOnePage($id){
+        
+         $qb = $this->createQueryBuilder('a');
 
   $qb->where('a.id = :id')
     ->setParameter('id', $id);
 
-  return $qb
+    return $qb
     ->getQuery()
     ->getResult() ;
-  
-}
+    }
     
 }
